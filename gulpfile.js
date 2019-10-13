c8b500c95993d7e5a5d29b50336f5ba0a81bb30c
Daggron <returnofking04@gmail.com>
2019-10-13 20:42:13 +0530

    copy task added

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,13 @@ const gulp = require('gulp');
 
 gulp.task('message',async ()=>{
    return console.log("Gulp is running");
-})
+});
 
 gulp.task('default',async()=>{
     return console.log('I am Here and running');
-})
+});
+
+gulp.task('copy',async ()=>{
+    gulp.src('src/*.html')
+        .pipe(gulp.dest('dist'))
+});
